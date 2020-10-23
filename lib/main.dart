cf521c19837825265c5d408e090c2d716ce516f0
FlutLab.io <>
2020-10-23 08:46:22 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> chips = [
+    Container(width: double.infinity),
+    Chip(label: Text('Kushal')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chips'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Wrap(
+          spacing: 8.0,
+          alignment: WrapAlignment.center,
+          runSpacing: 4.0,
+          children: chips,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            chips.add(Chip(label: Text('Kushala')));
+          });
+        },
+      ),
+    );
+  }
+}
